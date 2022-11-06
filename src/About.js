b0dfb9f80806f3459a2840f80a67d2ca55744087
Kalilah Clarke <thepursuit@Kalilahs-MacBook-Pro.local>
2022-11-05 22:03:56 -0400

    About Changes Made

--- src/About.js
+++ src/About.js
@@ -5,7 +5,117 @@ const About = () => {
     return (
         <div className="About">
             <h3>Hackers Team</h3>
-            <section>
+            return (
+    <article>
+      <h1>About Project</h1>
+      <p></p>
+
+      <h2>Hacker Team</h2>
+      <div className="collaborators">
+        <section className="Samatha Figueroa">
+          <img
+            src="https://media-exp1.licdn.com/dms/image/C4E03AQECVVt3NEUc1w/profile-displayphoto-shrink_800_800/0/1640218996877?e=1673481600&v=beta&t=-DRqmPzuks0Ph2yCEmF3wuD5VRVtePXyOqwcNv5RwgM"
+            alt="Samantha Figueroa Profile"
+          />
+          <h3>Samantha Figueroa</h3>
+          <a
+            href="https://www.linkedin.com/in/samantha-figueroa-fs/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/LinkedIn_icon_circle.svg/2048px-LinkedIn_icon_circle.svg.png"
+              alt="LinkedIn"
+            />
+          </a>
+          <a
+            href="https://github.com/SamFigueroa92"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+              alt="GitHub"
+            />
+          </a>
+
+          <p> <strong>8.4 Pursuit Fellow</strong> <br/>
+          Add Your Description 
+          </p>
+        </section>
+
+        <section className='KalilahClarke'>
+          <img
+            src="https://avatars.githubusercontent.com/u/75289632?v=4"
+            alt="Kalilah Clarke Profile"
+          />
+          <h3>Kalilah Clarke</h3>
+          <a
+            href="https://www.linkedin.com/in/kalilah-clarke-36876530/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/LinkedIn_icon_circle.svg/2048px-LinkedIn_icon_circle.svg.png"
+              alt="LinkedIn"
+            />
+          </a>
+          <a
+            href="https://github.com/KalilahClarke"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+              alt="GitHub"
+            />
+          </a>
+          <p>
+            <b>8.4 Pursuit Fellow</b> <br /> Fluent in Javascript React,
+            learning one language at a time. Using her Education background she
+            endeavors to create Applications that are simplistic and clean; that
+            are used to simplify daily life. <strong>Adaptability</strong> is
+            the name of her Game.
+          </p>
+        </section>
+
+        <section className='DerekPeterson'>
+          <img
+            src="https://media-exp1.licdn.com/dms/image/C4E03AQHrQhk0MBWm2A/profile-displayphoto-shrink_800_800/0/1661121871117?e=1673481600&v=beta&t=jlWtR7ei-nuHJLdgqoXngFEYHxevv-yoGJdzBuFytZI"
+            alt="Derek Peterson"
+          />
+          <h3>Derek Peterson</h3>
+          <a
+            href="https://www.linkedin.com/in/derekpeterson-plutus/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/LinkedIn_icon_circle.svg/2048px-LinkedIn_icon_circle.svg.png"
+              alt="LinkedIn"
+            />
+          </a>
+          <a
+            href="https://github.com/derekpeterson-plutus"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+              alt="GitHub"
+            />
+          </a>
+          <p>
+            <b>8.4 Pursuit Fellow</b> <br />
+          </p>
+        </section>
+      </div>
+    </article>
+  );
+
+            {/* <section>
             <h3>Samantha</h3>
             <a href='https://www.linkedin.com/in/samantha-figueroa-fs/' target='_blank' rel='noreferrer'>
             <BsLinkedin/>
@@ -31,7 +141,7 @@ const About = () => {
             <a href='https://github.com/derekpeterson-plutus' target='_blank' rel='noreferrer'>
             <BsGithub/>
             </a>
-            </section>
+            </section> */}
         </div>
     );
 };
